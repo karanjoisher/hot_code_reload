f558de2d06b4f7f50f15c727e45c810031e97297
Karan Joisher <karanjoisher@gmail.com>
2021-05-13 14:27:16 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,17 +1,13 @@
 @echo off
 
-IF NOT EXIST ./build mkdir ./build
-cd build
+if not exist build\. mkdir build
+pushd build
 
 set FLAGS=-DPLATFORM_WINDOWS=1
 set CommonCompilerFlags=-MTd -Gm- -nologo -GR- -EHa- -Oi  -W4 -wd4201 -wd4100 -wd4127 -wd4505 -wd4189 -fp:fast  /Z7 
 
 set CommonLinkerFlags=user32.lib Gdi32.lib winmm.lib Ole32.lib
 
-cl  %FLAGS% %CommonCompilerFlags% W:/hot_code_reload/hot_code_reload_launcher.cpp /link    %CommonLinkerFlags% -incremental:no
+cl  %FLAGS% %CommonCompilerFlags% ..\hot_code_reload_launcher.cpp /link %CommonLinkerFlags% -incremental:no
 
 cd ..
-
-
-
-
